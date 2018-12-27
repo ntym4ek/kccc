958c0562d2227226c1332d71330af3207565de2e
ntym4ek <mega_comp@mail.ru>
2018-12-27 10:13:58 +0300

    правки

--- sites/all/modules/_/product_extension/js/mini_calc.js
+++ sites/all/modules/_/product_extension/js/mini_calc.js
@@ -22,59 +22,61 @@
                 var $price1 = $('#price_1').val();
                 var $area = $('.calculation input').val();
                 var $rate_str = $('.rate.active').html();
-                var $rate_arr = [];
-                if ($price1 === undefined) {
-                    $price1 = $price0;
-                    $rate_arr[0] = $rate_str.split('-');
-                } else {
-                    $rate_arr = $rate_str.split('+');
-                    $rate_arr[0] = $rate_arr[0].split('-');
-                    $rate_arr[1] = $rate_arr[1].split('-');
-                }
-
-                $rate_arr[0][0] = $rate_arr[0][0].replace(',', '.');
-                $rate_arr[0][0] = parseFloat($rate_arr[0][0]) * mult_rate;
-                var $rate_from0  = accounting.formatNumber($area * $rate_arr[0][0] * mult_volume, 0, " ");
-                var $price_from0 = accounting.formatNumber($area * $rate_arr[0][0] * $price0, 2, " ");
-                var $rate_to0 = "", $price_to0 = "";
-                if ($rate_arr[0].length > 1) {
-                    $rate_arr[0][1] = $rate_arr[0][1].replace(',', '.');
-                    $rate_arr[0][1] = parseFloat($rate_arr[0][1]) * mult_rate;
-                    $rate_to0 = accounting.formatNumber($area * $rate_arr[0][1] * mult_volume, 0, " ");
-                    $price_to0 = accounting.formatNumber($area * $rate_arr[0][1] * $price0, 2, " ");
-                }
-                var $rate0 = $rate_from0 + ($rate_to0 ? ' - ' + $rate_to0 : '');
-                var $amount0 = $price_from0 + ($price_to0 ? " - " + $price_to0 : "") + " руб.";
-
-                var $rate1 = "", $amount1 = "";
-                if ($rate_arr[1] !== undefined) {
-                    $rate_arr[1][0] = $rate_arr[1][0].replace(',', '.');
-                    $rate_arr[1][0] = parseFloat($rate_arr[1][0]) * mult_rate;
-                    var $rate_from1 = accounting.formatNumber($area * $rate_arr[1][0] * mult_volume, 0, " ");
-                    var $price_from1 = accounting.formatNumber($area * $rate_arr[1][0] * $price1, 2, " ");
-                    var $rate_to1 = "", $price_to1 = "";
-                    if ($rate_arr[1][1] !== undefined) {
-                        $rate_arr[1][0] = $rate_arr[1][0].replace(',', '.');
-                        $rate_arr[1][1] = parseFloat($rate_arr[1][1]);
-                        $rate_to1 = accounting.formatNumber($area * $rate_arr[1][1] * mult_volume, 0, " ");
-                        $price_to1 = accounting.formatNumber($area * $rate_arr[1][1] * $price1, 2, " ");
+                if ($rate_str) {
+                    var $rate_arr = [];
+                    if ($price1 === undefined) {
+                        $price1 = $price0;
+                        $rate_arr[0] = $rate_str.split('-');
+                    } else {
+                        $rate_arr = $rate_str.split('+');
+                        $rate_arr[0] = $rate_arr[0].split('-');
+                        $rate_arr[1] = $rate_arr[1].split('-');
                     }
-                    $rate1 = $rate_from1 + ($rate_to1 ? ' - ' + $rate_to1 : '');
-                    $amount1 = $price_from1 + ($price_to1 ? ' - ' + $price_to1 : '') + ' руб.';
+
+                    $rate_arr[0][0] = $rate_arr[0][0].replace(',', '.');
+                    $rate_arr[0][0] = parseFloat($rate_arr[0][0]) * mult_rate;
+                    var $rate_from0 = accounting.formatNumber($area * $rate_arr[0][0] * mult_volume, 0, " ");
+                    var $price_from0 = accounting.formatNumber($area * $rate_arr[0][0] * $price0, 2, " ");
+                    var $rate_to0 = "", $price_to0 = "";
+                    if ($rate_arr[0].length > 1) {
+                        $rate_arr[0][1] = $rate_arr[0][1].replace(',', '.');
+                        $rate_arr[0][1] = parseFloat($rate_arr[0][1]) * mult_rate;
+                        $rate_to0 = accounting.formatNumber($area * $rate_arr[0][1] * mult_volume, 0, " ");
+                        $price_to0 = accounting.formatNumber($area * $rate_arr[0][1] * $price0, 2, " ");
+                    }
+                    var $rate0 = $rate_from0 + ($rate_to0 ? ' - ' + $rate_to0 : '');
+                    var $amount0 = $price_from0 + ($price_to0 ? " - " + $price_to0 : "") + " руб.";
+
+                    var $rate1 = "", $amount1 = "";
+                    if ($rate_arr[1] !== undefined) {
+                        $rate_arr[1][0] = $rate_arr[1][0].replace(',', '.');
+                        $rate_arr[1][0] = parseFloat($rate_arr[1][0]) * mult_rate;
+                        var $rate_from1 = accounting.formatNumber($area * $rate_arr[1][0] * mult_volume, 0, " ");
+                        var $price_from1 = accounting.formatNumber($area * $rate_arr[1][0] * $price1, 2, " ");
+                        var $rate_to1 = "", $price_to1 = "";
+                        if ($rate_arr[1][1] !== undefined) {
+                            $rate_arr[1][0] = $rate_arr[1][0].replace(',', '.');
+                            $rate_arr[1][1] = parseFloat($rate_arr[1][1]);
+                            $rate_to1 = accounting.formatNumber($area * $rate_arr[1][1] * mult_volume, 0, " ");
+                            $price_to1 = accounting.formatNumber($area * $rate_arr[1][1] * $price1, 2, " ");
+                        }
+                        $rate1 = $rate_from1 + ($rate_to1 ? ' - ' + $rate_to1 : '');
+                        $amount1 = $price_from1 + ($price_to1 ? ' - ' + $price_to1 : '') + ' руб.';
+                    }
+
+                    var $calc_volume = 0, $calc_cost = 0;
+                    if ($rate_arr[1] !== undefined && $rate_arr[1][1] !== undefined) {
+                        $calc_volume = $rate0 + '<br />+ ' + $rate1;
+                        $calc_cost = $amount0 + '<br />+ ' + $amount1;
+                    } else {
+                        $calc_volume = $rate0 + ($rate1 ? ' + ' + $rate1 : '');
+                        $calc_cost = $amount0 + ($amount1 ? ' + ' + $amount1 : '');
+                    }
+
+
+                    $('.calc-volume').html($calc_volume);
+                    $('.calc-cost').html($calc_cost);
                 }
-
-                var $calc_volume = 0, $calc_cost = 0;
-                if ($rate_arr[1] !== undefined && $rate_arr[1][1] !== undefined) {
-                    $calc_volume = $rate0 + '<br />+ ' + $rate1;
-                    $calc_cost = $amount0 + '<br />+ ' + $amount1;
-                } else {
-                    $calc_volume = $rate0 + ($rate1 ? ' + ' + $rate1 : '');
-                    $calc_cost = $amount0 + ($amount1 ? ' + ' + $amount1 : '');
-                }
-
-
-                $('.calc-volume').html($calc_volume);
-                $('.calc-cost').html($calc_cost);
             }
             $(document).ready( function(){
                 evaluate();
