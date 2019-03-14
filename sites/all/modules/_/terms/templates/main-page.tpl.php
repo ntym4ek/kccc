f61c7bc886411aea9ae2e9f842b2e5ad16f82f5b
ntym4ek <mega_comp@mail.ru>
2019-03-14 13:15:53 +0300

    правки

--- sites/all/modules/_/terms/templates/main-page.tpl.php
+++ sites/all/modules/_/terms/templates/main-page.tpl.php
@@ -2,28 +2,30 @@
 <?php endif; ?>
 <div class="terms">
     <div class="filter">
-        <?php foreach ($terms as $key_l => $letter): ?>
-            <div class="letter"><a href="#<?php print translit($key_l); ?>"><?php print $key_l; ?></a></div>
-        <?php endforeach; ?>
+        <ul class="letters">
+            <?php foreach ($terms as $key_l => $letter): ?>
+                <li class="letter"><a href="#<?php print translit($key_l); ?>"><?php print $key_l; ?></a></li>
+            <?php endforeach; ?>
+        </ul>
     </div>
     <?php foreach ($terms as $key_l => $litera): ?>
         <div class="litera-row">
             <div id="<?php print translit($key_l); ?>" class="litera"><?php print $key_l; ?></div>
             <div class="items">
-            <?php foreach ($litera as $key=>$term): ?>
-                <div class="item">
-                    <div class="mean-wrap">
-                        <div class="mean">
-                            <div class="head"><?php print $term['ru']; ?></div>
-                            <span class="sub"><?php print $term['sub']; ?></span>
-                            <?php print $term['mean']; ?>
-                            <span class="close">×</span>
+                <?php foreach ($litera as $key=>$term): ?>
+                    <div class="item">
+                        <div class="mean-wrap">
+                            <div class="mean">
+                                <div class="head"><?php print $term['ru']; ?></div>
+                                <span class="sub"><?php print $term['sub']; ?></span>
+                                <?php print $term['mean']; ?>
+                                <span class="close">×</span>
+                            </div>
                         </div>
+                        <h3><a class="ru"><?php print $term['ru']; ?></a></h3>
+                        <div class="en"><?php print $term['en']; ?></div>
                     </div>
-                    <h3><a class="ru"><?php print $term['ru']; ?></a></h3>
-                    <div class="en"><?php print $term['en']; ?></div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
             </div>
         </div>
         <div class="delimiter"></div>
