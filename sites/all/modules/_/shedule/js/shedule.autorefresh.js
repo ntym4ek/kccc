f2d1e8ddaaacc2db1250ea6dc98283e534dba97d
ntym4ek <mega_comp@mail.ru>
2019-10-21 13:05:24 +0300

    небольшой фикс

--- sites/all/modules/_/shedule/js/shedule.autorefresh.js
+++ sites/all/modules/_/shedule/js/shedule.autorefresh.js
@@ -1,19 +1,19 @@
 (function($){
     Drupal.behaviors.SheduleAutoRefresh = {
         attach: function (context, settings) {
-            // $("body").once(function () {
-            //     var ajax = new Drupal.ajax(
-            //             false,
-            //             false,
-            //             { url : "/shedule/ajax" }
-            //         );
-            //     var timer = window.setInterval(function(){
-            //         $(document).queue(function() {
-            //             ajax.eventResponse(ajax, {});
-            //             $(document).dequeue();
-            //         });
-            //         return false; }, 5000);
-            // });
+            $("body").once(function () {
+                var ajax = new Drupal.ajax(
+                        false,
+                        false,
+                        { url : "/shedule/ajax" }
+                    );
+                var timer = window.setInterval(function(){
+                    $(document).queue(function() {
+                        ajax.eventResponse(ajax, {});
+                        $(document).dequeue();
+                    });
+                    return false; }, 5000);
+            });
         }
     };
 })(jQuery);
