0cf6fe4f3e3981acc546b2f2d2657c8a1453b440
ntym4ek <mega_comp@mail.ru>
2019-11-15 10:31:31 +0300

    перевод карты в Обратной связи

--- sites/all/modules/_/ext_entityform/js/map.js
+++ sites/all/modules/_/ext_entityform/js/map.js
@@ -18,7 +18,7 @@
                     myPlacemarkWithContent = new ymaps.Placemark([58.540547, 49.976694], {
                         hintContent: 'Административное здание',
                         balloonContent: 'Пн-Пт  8:00-17:00<br />+7 (8332) 76-15-20',
-                        iconContent: Drupal.t("OOO Trade House") + "<br />" + Drupal.t("\"Kirovo-Chepetsk Chemical Company\"")
+                        iconContent: Drupal.t("OOO Trade House") + "<br />" + Drupal.t('"Kirovo-Chepetsk Chemical Company"')
                     }, {
                         iconLayout: 'default#imageWithContent',
                         iconImageHref: '/sites/all/modules/_/ext_entityform/images/map_logo.png',
--- sites/all/modules/_/ext_entityform/templates/contact-us-entityform.tpl.php
+++ sites/all/modules/_/ext_entityform/templates/contact-us-entityform.tpl.php
@@ -33,8 +33,6 @@
             </fieldset>
         </div>
     </div>
-    <div id="map" class="cu-map col-xs-12">
-<!--        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3AdHxIteY-9AGYj5ZaIUEB86qWoAvLQXwJ&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>-->
-    </div>
+    <div id="map" class="cu-map col-xs-12"></div>
 </div>
 
