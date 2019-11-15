4d716c43ee194db435f2a3734d965ce49071b99b
ntym4ek <mega_comp@mail.ru>
2019-11-15 13:40:31 +0300

    перевод карты в Обратной связи

--- sites/all/modules/_/ext_entityform/js/map.js
+++ sites/all/modules/_/ext_entityform/js/map.js
@@ -22,7 +22,7 @@
                     }, {
                         iconLayout: 'default#imageWithContent',
                         iconImageHref: '/sites/all/modules/_/ext_entityform/images/map_logo.png',
-                        iconImageSize: [45,47],
+                        iconImageSize: [42,47],
                         iconImageOffset: [-30, -30],
                         iconContentSize: [275,33],
                         iconContentOffset: [50, 2],
