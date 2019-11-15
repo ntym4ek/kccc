92cd172c12ee7128b56c39516cc3b62282e5e850
ntym4ek <mega_comp@mail.ru>
2019-11-15 08:54:21 +0300

    смена карты в Контактах

--- sites/all/modules/_/ext_entityform/templates/contact-us-entityform.tpl.php
+++ sites/all/modules/_/ext_entityform/templates/contact-us-entityform.tpl.php
@@ -8,7 +8,7 @@
         <div class="cu-title"><? print t('Address'); ?></div>
         <div class="cu-content"><? print t('613048, Kirov region, Kirovo-Chepetsk, Proizvodstvennaya, 6'); ?></div>
         <div class="cu-title"><? print t('Phone'); ?></div>
-        <div class="cu-content"><? print '+7 (8332) 76-15-20 доб. 1107'; ?><a href="tel:+78332761520"><i class="icon-phone"></i><? print t('Call');?></a></div>
+        <div class="cu-content"><? print '+7 (8332) 76-15-20 ' . t('add.') . ' 1107'; ?><a href="tel:+78332761520"><i class="icon-phone"></i><? print t('Call');?></a></div>
         <div class="cu-title"><? print 'Email'; ?></div>
         <div class="cu-content eAddr-encoded">e(td[s1]kc/cc[s2]ru)<a href="e(td[s1]kc/cc[s2]ru)" class="eAddr-encoded"><i class="icon-mail"></i><? print t('Email us');?></a></div>
         <div class="hr"></div>
@@ -33,8 +33,8 @@
             </fieldset>
         </div>
     </div>
-    <div class="cu-map col-xs-12">
-        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3AdHxIteY-9AGYj5ZaIUEB86qWoAvLQXwJ&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
+    <div id="map" class="cu-map col-xs-12">
+<!--        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3AdHxIteY-9AGYj5ZaIUEB86qWoAvLQXwJ&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>-->
     </div>
 </div>
 
