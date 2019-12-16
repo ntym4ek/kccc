537ffe1e1a6718b078c3a895cbc5adad7e52ef04
ntym4ek <mega_comp@mail.ru>
2019-12-16 14:09:29 +0300

    страница с видеоматериалами

--- sites/all/themes/gard/templates/system/router-card.tpl.php
+++ sites/all/themes/gard/templates/system/router-card.tpl.php
@@ -5,16 +5,16 @@
     <div class="r-card field">
 
         <div class="r-card-image">
-            <a href="<?php print $item['url']; ?>">
-                <img src="<?php print $item['image_url']; ?>" class="img-responsive" title="Действие препарата в поле">
+            <a href="<?php print $item['url']; ?>"<?php print empty($item['nofollow']) ? '' : ' rel="nofollow"';?>>
+                <img src="/<?php print $item['image_url']; ?>" class="img-responsive" title="<?php print $item['title']; ?>">
             </a>
         </div>
 
         <div class="r-card-content">
-            <h4 class="r-card-title"><a href="<?php print $item['url']; ?>"><?php print $item['title']; ?></a></h4>
+            <h4 class="r-card-title"><a href="<?php print $item['url']; ?>"<?php print empty($item['nofollow']) ? '' : ' rel="nofollow"';?>><?php print $item['title']; ?></a></h4>
             <p class="r-card-description"><?php print $item['description']; ?></p>
 
-            <a class="btn btn-danger r-card-more" href="<?php print $item['url']; ?>">
+            <a class="btn btn-danger r-card-more" href="<?php print $item['url']; ?>"<?php print empty($item['nofollow']) ? '' : ' rel="nofollow"';?>>
                 <?php print empty($item['button_text']) ? t('More') : $item['button_text']; ?>
                 <i class="fa fa-chevron-right pull-right"></i>
             </a>
