629df81aef17aad7bbfbc2e332ebc623668be500
ntym4ek <mega_comp@mail.ru>
2020-07-14 08:58:14 +0300

    too many redirects

--- sites/all/themes/gard/templates/page.tpl.php
+++ sites/all/themes/gard/templates/page.tpl.php
@@ -129,7 +129,7 @@
             <header class="row content-header" role="banner" >
                 <div class="category-header hidden-print">
                     <?php if (!empty($header['image'])):?>
-                    <img src="<?php print $header['image']; ?>">
+                    <img src="<?php print $header['image']; ?>" alt="<?php print $header['category_title']; ?>">
                     <? endif; ?>
                     <?php if (!empty($header['category_title'])):?>
                     <div class="category-title">
