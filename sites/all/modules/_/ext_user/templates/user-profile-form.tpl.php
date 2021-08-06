e46e5e2ed7232fb1837af1682263f1e66975b590
ntym4ek <mega_comp@mail.ru>
2021-08-06 13:59:53 +0300

    возможность добавления профиля компании в ЛК

--- sites/all/modules/_/ext_user/templates/user-profile-form.tpl.php
+++ sites/all/modules/_/ext_user/templates/user-profile-form.tpl.php
@@ -5,35 +5,23 @@
       <div class="row">
         <div class="profile-left">
           <div class="photo">
+
             <? if (isset($form["profile_main"]["field_profile_photo"])): ?>
               <? echo render($form["profile_main"]["field_profile_photo"]); ?>
             <? endif; ?>
 
-            <? if (isset($form['picture'])): ?>
-              <? echo render($form['picture']); ?>
-            <? endif; ?>
           </div>
         </div>
 
         <div class="profile-right">
-          <? if (isset($form["group_g1"])): ?>
-            <? echo render($form['group_g1']); ?>
-          <? endif; ?>
 
-          <? if (isset($form["profile_main"])): ?>
-            <? echo render($form["profile_main"]); ?>
-          <? endif; ?>
-
-          <? if (isset($form["profile_staff"])): ?>
-            <? echo render($form["profile_staff"]); ?>
-          <? endif; ?>
+          <? hide($form["actions"]); ?>
+          <? echo drupal_render_children($form); ?>
 
           <? echo render($form["actions"]); ?>
+
         </div>
       </div>
-
-      <?php echo drupal_render_children($form); ?>
-
     </div>
   </div>
 </div>
