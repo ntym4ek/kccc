9a0a45f453c5290cda89138e9310ab6e02abe649
ntym4ek <mega_comp@mail.ru>
2021-09-16 09:57:06 +0300

    гео контакты на странице Контакты

--- sites/all/modules/_/ext_entityform/js/map.js
+++ sites/all/modules/_/ext_entityform/js/map.js
@@ -1,27 +1,28 @@
 (function ($) {
-    Drupal.behaviors.Map = {
+    Drupal.behaviors.ext_entityform = {
         attach: function (context, settings) {
 
+          var contact = settings.ext_entityform.contact;
             ymaps.ready(function () {
-                var myMap = new ymaps.Map('map', {
-                        center: [58.538202, 50.009723],
+                var myMap = new ymaps.Map("map", {
+                        center: [contact.coords.x, contact.coords.y],
                         zoom: 13
                     }, {
-                        searchControlProvider: 'yandex#search'
+                        searchControlProvider: "yandex#search"
                     }),
 
                     // Создаём макет содержимого.
                     MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-                        '<div style="color: #000; background: #fff; border: 2px solid #0DB02B; border-radius: 5px; padding: 5px; ">$[properties.iconContent]</div>'
+                        "<div style=\"color: #000; background: #fff; border: 2px solid #0DB02B; border-radius: 5px; padding: 5px; \">$[properties.iconContent]</div>"
                     ),
 
-                    myPlacemarkWithContent = new ymaps.Placemark([58.540547, 49.976694], {
-                        hintContent: 'Административное здание',
-                        balloonContent: 'Пн-Пт  8:00-17:00<br />+7 (8332) 76-15-20',
-                        iconContent: Drupal.t("OOO Trade House") + "<br />" + Drupal.t('"Kirovo-Chepetsk Chemical Company"')
+                    myPlacemarkWithContent = new ymaps.Placemark([contact.coords.x, contact.coords.y], {
+                        hintContent: "Административное здание",
+                        balloonContent: contact.schedule + "<br />" + contact.phone_txt,
+                        iconContent: Drupal.t("OOO Trade House") + "<br />" + Drupal.t("Kirovo-Chepetsk Chemical Company")
                     }, {
-                        iconLayout: 'default#imageWithContent',
-                        iconImageHref: '/sites/all/modules/_/ext_entityform/images/map_logo.png',
+                        iconLayout: "default#imageWithContent",
+                        iconImageHref:"/sites/all/modules/_/ext_entityform/images/map_logo.png",
                         iconImageSize: [42,47],
                         iconImageOffset: [-30, -30],
                         iconContentSize: [275,33],
