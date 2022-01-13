114239c78bd5373eb5095e771d68b285396a2143
git-seo <86597232+git-seo@users.noreply.github.com>
2022-01-13 17:07:17 +0700

    Изменение кнопки подробнее
    Изменена кнопка "Подробнее" на "Купить"

--- sites/all/themes/gard/templates/node/node--product-agro--teaser.tpl.php
+++ sites/all/themes/gard/templates/node/node--product-agro--teaser.tpl.php
@@ -94,7 +94,7 @@
             <div><?php print $content['body'][0]['#markup']; ?></div>
         </div>
         <a class="btn btn-primary v-card-more" href="<?php print $product_url; ?>">
-            <?php print t('More'); ?>
+            <?php print t('Купить'); ?>
             <i class="fas fa-chevron-right pull-right"></i>
         </a>
     </div>
