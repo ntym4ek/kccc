1b55c92cc3e00a68790a8afa10520be4b2326276
git-seo <86597232+git-seo@users.noreply.github.com>
2022-01-13 18:27:11 +0700

    Баковые смеси - кнопка купить

--- sites/all/themes/gard/templates/node/node--product-mix--teaser.tpl.php
+++ sites/all/themes/gard/templates/node/node--product-mix--teaser.tpl.php
@@ -97,7 +97,7 @@
             <div><?php print $summary; ?></div>
         </div>
         <a class="btn btn-primary v-card-more" href="<?php print $product_url; ?>">
-            <?php print t('More'); ?>
+            <?php print t('Купить'); ?>
             <i class="fas fa-chevron-right pull-right"></i>
         </a>
     </div>
