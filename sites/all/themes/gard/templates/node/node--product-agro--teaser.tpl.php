c9a90eb7d943f2ff35b6363d58bb5d5422122525
git-seo <86597232+git-seo@users.noreply.github.com>
2022-01-14 11:46:33 +0700

    Изменение формата цены на основных категориях

--- sites/all/themes/gard/templates/node/node--product-agro--teaser.tpl.php
+++ sites/all/themes/gard/templates/node/node--product-agro--teaser.tpl.php
@@ -101,7 +101,7 @@
 
     <footer>
         <?php if (!empty($content['field_pd_price_per_unit']['#items'][0]['amount'])): ?>
-            <?php print '<strong>' . t('Price per ') . ' ' . mb_strtolower($content['field_pd_units'][0]['#markup']) . '</strong> ' . t('(with NDS)') . ': ' . $content['field_pd_price_per_unit'][0]['#markup']; ?>
+            <?php print '<span class="price_field">Цена: ' . $content['field_pd_price_per_unit'][0]['#markup'] .' за '.mb_strtolower($content['field_pd_units'][0]['#markup']).' (с НДС)</span>'; ?>
         <?php endif; ?>
     </footer>
 </div>
