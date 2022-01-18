2a7dc25ae3f220e4d066ef5b0ed7d604fc534b91
ntym4ek <mega_comp@mail.ru>
2022-01-18 09:13:44 +0300

    Правки по изменениям от git-seo

--- sites/all/themes/gard/templates/node/node--product-agro--teaser.tpl.php
+++ sites/all/themes/gard/templates/node/node--product-agro--teaser.tpl.php
@@ -94,14 +94,14 @@
             <div><?php print $content['body'][0]['#markup']; ?></div>
         </div>
         <a class="btn btn-primary v-card-more" href="<?php print $product_url; ?>">
-            <?php print t('Купить'); ?>
+            <?php print t('Buy'); ?>
             <i class="fas fa-chevron-right pull-right"></i>
         </a>
     </div>
 
     <footer>
         <?php if (!empty($content['field_pd_price_per_unit']['#items'][0]['amount'])): ?>
-            <?php print '<span class="price_field">Цена: ' . $content['field_pd_price_per_unit'][0]['#markup'] .' за '.mb_strtolower($content['field_pd_units'][0]['#markup']).' (с НДС)</span>'; ?>
+            <?php print '<span class="price_field">' . t('Price') . ': ' . $content['field_pd_price_per_unit'][0]['#markup'] . ' ' . t('per') . ' ' . mb_strtolower($content['field_pd_units'][0]['#markup']) . ' ' . t('(with NDS)') . '</span>'; ?>
         <?php endif; ?>
     </footer>
 </div>
