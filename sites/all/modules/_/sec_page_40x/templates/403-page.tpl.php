76d5544bdc71173992a3001f6c5260ffef865bba
ntym4ek <mega_comp@mail.ru>
2022-07-22 15:28:59 +0300

    Модуль вывода страниц 40х с возможностью подменять содержимое или переадресации.

---
+++ sites/all/modules/_/sec_page_40x/templates/403-page.tpl.php
@@ -0,0 +1,17 @@
+
+<div class="page40x">
+    <div class="b1">
+        <h1>403</h1>
+        <h2><?php print t('Access denied'); ?></h2>
+        <h4><?php print t('But still you could be interested in:'); ?></h4>
+        <ul>
+            <li><?php print l(t('Preparations catalog'), 'catalog/agrochemicals'); ?></li>
+            <li><?php print l(t('Protection programs'), 'handbook/protection-programs'); ?></li>
+            <li><?php print l(t('Agro Calculator'), 'agro-calculator'); ?></li>
+        </ul>
+        <a href="<?php print $GLOBALS['base_root']; ?>" class="btn btn-success btn-wide"><?php print t('Homepage'); ?></a>
+    </div>
+    <div class="b2">
+        <img src="/sites/all/modules/_/sec_page_40x/images/403.jpg" class="img-responsive" alt="">
+    </div>
+</div>
