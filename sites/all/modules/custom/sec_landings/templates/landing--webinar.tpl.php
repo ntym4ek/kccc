2d282293e23f1a34167e82a38989e33f081d38c6
ntym4ek <mega_comp@mail.ru>
2023-10-25 15:49:32 +0300

    добавлен модуль Лендингов

---
+++ sites/all/modules/custom/sec_landings/templates/landing--webinar.tpl.php
@@ -0,0 +1,112 @@
+<div class="content-container">
+  <div class="container-fluid">
+    <div class="row landing webinar">
+
+      <div class="row-1">
+        <div class="logo">
+          <img src="/sites/all/modules/_/sec_landings/images/webinar/logo.png" alt="KCCC GROUP">
+        </div>
+        <div class="webinar">
+          <img src="/sites/all/modules/_/sec_landings/images/webinar/webinar.png" alt="Вебинар KCCC">
+        </div>
+        <div class="date">
+          <img src="/sites/all/modules/_/sec_landings/images/webinar/date1.png" alt="Вебинар 14 января">
+        </div>
+
+        <div class="image">
+          <img src="/sites/all/modules/_/sec_landings/images/webinar/room.png" alt="KCCC GROUP">
+        </div>
+        <div class="text">
+          <div class="theme">Основная тема</div>
+          <div class="title">Защита озимых<br>и яровых колосовых<br>в 2023 г.</div>
+          <div class="button"><a href="https://events.webinar.ru/67894613/1333605982?utm_source=webinar_140223&utm_medium=site" class="btn">Регистрируйся!</a></div>
+        </div>
+      </div>
+
+      <div class="row-2">
+        <div class="row-21">
+          <div class="text">
+            <h3>Уважаемые коллеги!</h3>
+            <p>Кирово-Чепецкая Химическая Компания приглашает вас принять участие в вебинаре «Защита озимых и яровых колосовых в 2023г.», который пройдет <span>17 февраля в 15:00 по МСК</span>.</p>
+            <p>В рамках трансляции специалисты компании и приглашенные спикеры расскажут об основных тенденциях в защите зерновых и ответят на Ваши вопросы.</p>
+          </div>
+          <div class="image">
+            <img src="/sites/all/modules/_/sec_landings/images/webinar/comp.png" alt="KCCC GROUP">
+          </div>
+        </div>
+
+        <div class="highlight">Для кого вебинар</div>
+
+        <div class="row-22">
+          <div class="block">Специалистов-агрономов, которые держат руку на пульсе последних изменений в методиках защиты зерновых<span></span></div>
+          <div class="block">Руководителей растениеводческих хозяйств, желающих повысить рентабельность производства<span></span></div>
+        </div>
+
+        <div class="row-23">
+          <div class="text">
+            <h3>Программа вебинара:</h3>
+            <ul>
+              <li>Прогноз основных рисков производства зерновых в 2023 г.</li>
+              <li>Фузариоз, септориоз и снежная плесень. Как эффективно контролировать появление и развитие болезней.</li>
+              <li>Новинки препаратов для защиты посевов. Современный подход и стратегия использования фунгицидов.</li>
+            </ul>
+          </div>
+          <div class="image">
+            <img src="/sites/all/modules/_/sec_landings/images/webinar/field.png" alt="KCCC GROUP">
+          </div>
+        </div>
+
+        <div class="highlight inverted">Спикеры</div>
+
+        <div class="row-24">
+          <div class="card">
+            <div class="photo">
+              <img src="/sites/all/modules/_/sec_landings/images/webinar/sirotkin_e.png" alt="KCCC GROUP">
+            </div>
+            <div class="name">Сироткин<br>Евгений Николаевич</div>
+            <div class="role">Эксперт по защите растений ООО ТД «Кирово-Чепецкая Химическая Компания»</div>
+          </div>
+          <div class="card">
+            <div class="photo">
+              <img src="/sites/all/modules/_/sec_landings/images/webinar/burlaka.png" alt="KCCC GROUP">
+            </div>
+            <div class="name">Бурлака<br>Галина Алексеевна</div>
+            <div class="role">Главный эксперт – консультант по болезням и вредителям растений Зернового Соевого Союза ПФО, кандидат биологических наук, энтомолог, фитопатолог.
+              Практика работы с болезнями и вредителями растений с 2002 года.</div>
+          </div>
+          <div class="card">
+            <div class="photo">
+              <img src="/sites/all/modules/_/sec_landings/images/webinar/bekasova.png" alt="KCCC GROUP">
+            </div>
+            <div class="name">Бекасова<br>Марина Викторовна</div>
+            <div class="role">Эксперт по защите растений ООО ТД «Кирово-Чепецкая Химическая Компания», кандидат сельскохозяйственных наук</div>
+          </div>
+        </div>
+
+        <div class="highlight">На вебинаре вас также ждут</div>
+
+        <div class="row-25">
+          <div class="block">
+            <div class="image"><img src="/sites/all/modules/_/sec_landings/images/webinar/question.png" alt="KCCC GROUP"></div>
+            <p>Ответы на ваши вопросы от эксперта</p>
+          </div>
+          <div class="block">
+            <div class="image"><img src="/sites/all/modules/_/sec_landings/images/webinar/gift.png" alt="KCCC GROUP"></div>
+            <p>Приятный бонус! Разыграем в прямом эфире 6 канистр препарата на опыты</p>
+          </div>
+        </div>
+
+        <div class="highlight inverted">Участвуйте в вебинаре – используйте шанс получить ценный приз!</div>
+
+        <div class="button">
+          <a href="https://events.webinar.ru/67894613/1333605982?utm_source=webinar_140223&utm_medium=site" class="btn">Регистрируйся!</a>
+        </div>
+
+        <div class="notice">
+          <p>Пожалуйста, зарегистрируйтесь, чтобы мы могли напомнить Вам о вебинаре и прислать ссылку для подключения.</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
