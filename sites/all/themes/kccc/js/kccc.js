76edfab11c069cfbaa4e8826e324deee996cd4a6
ntym4ek <mega_comp@mail.ru>
2023-11-02 10:58:04 +0300

    скролл мышкой подменю на десктопах

--- sites/all/themes/kccc/js/kccc.js
+++ sites/all/themes/kccc/js/kccc.js
@@ -7,7 +7,27 @@
         $(e.target).closest(".share").toggleClass("open");
       });
 
+      // -- скролл мышкой подменю на десктопах
+      $(".page-highlighted .sub-menu").mousedown(function(event) {
+        let $this = $(this),
+          startX = event.pageX - $this.offset().left,
+          scrollLeft = $this.scrollLeft();
 
+        $this.data('mouseMoveHandler', function(event) {
+          event.preventDefault();
+          let x = event.pageX - $this.offset().left,
+            walk = (x - startX) * 2;
+          $this.scrollLeft(scrollLeft - walk);
+        });
+
+        $(document).mousemove($this.data('mouseMoveHandler'));
+
+        $(document).mouseup(function() {
+          $(document).off('mousemove', $this.data('mouseMoveHandler'));
+        });
+      });
+
+      // -- аккордеон пунктов в мобильном меню
       class MobileMenuAccordion {
         constructor(target, config) {
           this._el = typeof target === "string" ? document.querySelector(target) : target;
