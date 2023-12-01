b27745c5e059f0b5baca185924af805de402117a
ntym4ek <mega_comp@mail.ru>
2023-12-01 15:03:41 +0300

    Правки оформления

---
+++ sites/all/modules/custom/sec_landings/templates/mail--webinar3.tpl.php
@@ -0,0 +1,7 @@
+<p><a href="https://kccc.ru/landing/webinar-sovremennye-podhody-k-vozdelyvaniyu-soi?utm_source=webinar_041223&utm_medium=kccc_sn">
+    <img src="https://kccc.ru/sites/all/modules/custom/sec_landings/images/webinar3/mail_800.jpg"></a>
+</p>
+<h2>Уважаемые коллеги!</h2>
+<p>Кирово-Чепецкая Химическая Компания приглашает вас принять участие в вебинаре «Современные подходы к возделыванию сои», который пройдет 04 декабря в 10:00.</p>
+<p>В рамках трансляции специалисты компании и приглашенные спикеры расскажут об основных тенденциях в возделывании сои и ответят на Ваши вопросы.</p>
+<p><a href="https://kccc.ru/landing/webinar-sovremennye-podhody-k-vozdelyvaniyu-soi?utm_source=webinar_041223&utm_medium=kccc_sn">Зарегистрируйтесь</a> на вебинар и будьте в курсе последних новостей рынка. До встречи в эфире!</p>
