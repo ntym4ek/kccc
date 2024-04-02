7261ab3baec47bf3f8086a472c3cfc349638aca8
ntym4ek <mega_comp@mail.ru>
2024-04-02 15:31:49 +0300

    правки

--- sites/all/themes/kccc/templates/html.tpl.php
+++ sites/all/themes/kccc/templates/html.tpl.php
@@ -43,7 +43,7 @@
  * @ingroup themeable
  */
 ?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces ?? ''; ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces ?? ''; ?>>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
@@ -52,9 +52,27 @@
   <?php print $scripts; ?>
 
   <?php if (!strpos($_SERVER['HTTP_HOST'], '.local')): ?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(11541151, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        ecommerce:"dataLayer"
+      });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/11541151" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 
     <?php // скрипт пикселя (Татьянох) ?>
-    <script type="text/javascript" src="//pixel.smr8.ru/metric/100502CC-24A2-4D8E-96D7-39B0A324C0C9" charset="UTF-8" async></script>
+<!--    <script type="text/javascript" src="//pixel.smr8.ru/metric/100502CC-24A2-4D8E-96D7-39B0A324C0C9" charset="UTF-8" async></script>-->
 
     <?php // отслеживание звонков через Calltouch (Татьянох) ?>
     <!-- calltouch -->
@@ -64,13 +82,13 @@
     <!-- calltouch -->
 
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){
-        w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-        var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;
-        j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-        f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-PWVNDPC');</script>
+<!--    <script>(function(w,d,s,l,i){-->
+<!--        w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});-->
+<!--        var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';-->
+<!--        j.async=true;-->
+<!--        j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;-->
+<!--        f.parentNode.insertBefore(j,f);-->
+<!--      })(window,document,'script','dataLayer','GTM-PWVNDPC');</script>-->
     <!-- End Google Tag Manager -->
   <?php endif;?>
 </head>
