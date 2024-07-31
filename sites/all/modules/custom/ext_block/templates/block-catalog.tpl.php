29994caa6b74a31f7e70f81174395b2283358344
ntym4ek <mega_comp@mail.ru>
2024-07-31 14:42:14 +0300

    правки блока категорий на главной

--- sites/all/modules/custom/ext_block/templates/block-catalog.tpl.php
+++ sites/all/modules/custom/ext_block/templates/block-catalog.tpl.php
@@ -10,17 +10,19 @@
     <div class="container">
       <div class="b1">
         <div class="row">
-          <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+          <div class="col-xs-12 col-md-10 col-md-offset-1">
             <div class="row">
 
               <?php $index = 1; ?>
               <?php foreach ($categories as $category): ?>
               <div class="col-xs-12 col-md-6">
                 <div class="category">
+                  <a href="<?php print $category['path']; ?>">
                   <div class="text">
-                    <div class="index"><?php print $index++; ?></div>
-                    <div class="title"><a href="<?php print $category['path']; ?>"><?php print $category['label']; ?></a></div>
+                    <div class="index"><i class="icon icon-<?php print $category["field_icon"]; ?>"></i></div>
+                    <div class="title"><?php print $category['label']; ?></div>
                   </div>
+                  </a>
                 </div>
               </div>
               <?php endforeach; ?>
