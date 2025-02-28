c1243cd3bf5b3cca98ebde00fcaadf459e74ed35
ntym4ek <mega_comp@mail.ru>
2025-02-28 15:49:40 +0300

    Popup Telegram

--- sites/all/modules/custom/ext_block/js/ext_block.js
+++ sites/all/modules/custom/ext_block/js/ext_block.js
@@ -7,5 +7,23 @@
     return "<span class=\"milestone " + this[1] + "\"><b>" + name + "</b></span>";
   };
 
+  Drupal.behaviors.ext_block = {
+    attach: function (context, settings) {
+
+      // --- Popup Telegram ----------------------------------------------------
+      setTimeout(()=> {
+        var popupTg = $.cookie('popup-tg');
+        if (!popupTg) {
+          $(".popup-tg-close").closest(".block-popup-tg").fadeIn(200);
+          $.cookie('popup-tg', true, { expires: 30 }); // 30 days
+        }
+      }, 5000);
+
+      $(".popup-tg-close, .block-popup-tg a").click(() => {
+        $(".popup-tg-close").closest(".block-popup-tg").fadeOut(200);
+      });
+    }
+  };
+
 })(jQuery);
 
---
+++ sites/all/modules/custom/ext_block/templates/block-popup-tg.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<div class="block-popup-tg">
+  <div class="popup-tg-bg"></div>
+  <div class="popup-tg-wr">
+    <div class="popup-tg">
+      <div class="popup-tg-close">X</div>
+      <div class="image">
+        <a href="https://t.me/tdkccc" target="_blank"><img src="/sites/default/files/images/blocks/kccc_telegram.png" alt="KCCC Telegram"></a>
+      </div>
+    </div>
+  </div>
+</div>
