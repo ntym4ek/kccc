93d75e6b006823c05ca57630d382401faef534d3
ntym4ek <mega_comp@mail.ru>
2025-03-05 16:40:40 +0300

    перенос в заголовке суффикса после торгового знака в верт. карточках на вторую строку

--- sites/all/themes/kccc/templates/card/card-product-v.tpl.php
+++ sites/all/themes/kccc/templates/card/card-product-v.tpl.php
@@ -7,7 +7,7 @@
       <img src="<?php print $product['image']['teaser']; ?>" alt="<?php print $product['title']; ?>">
     </div>
     <div class="text">
-      <div class="title"><?php print $product['label']; ?></div>
+      <div class="title"><?php print $product['label_card']; ?></div>
       <div class="components"><?php print $product['components']['formatted']; ?></div>
       <div class="more btn btn-brand btn-full-wide"><?php print t('Read more'); ?></div>
     </div>
